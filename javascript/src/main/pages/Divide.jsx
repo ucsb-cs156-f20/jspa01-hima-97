583dd53e9a449d5aab2aeff309d732cba29cad02
Himanshu Kumar <hkumar@ucsb.edu>
2020-10-14 17:31:39 -0700

    hk - fixed all failing tests

--- javascript/src/main/pages/Divide.jsx
+++ javascript/src/main/pages/Divide.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// 1
+import { divideRationalsFromUserInput } from "main/utils/RationalHelpers";
+import Calculator from "main/components/Calculator";
 
 const Divide = () => {
-  return <div>Div</div>;
+  const props = {
+    // 3
+    calculateFromUserInput: divideRationalsFromUserInput,
+    title: "Divide",
+    subtitle:
+      "Fill in the numerator and denominator of two rational numbers (fractions) below, then click the Calculate button to see the result.",
+    sign: "/",
+  };
+  // 4
+  return <Calculator {...props} />;
 };
 
 export default Divide;
--- javascript/src/main/pages/Subtract.jsx
+++ javascript/src/main/pages/Subtract.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { subtractRationalsFromUserInput } from "main/utils/RationalHelpers";
+import Calculator from "main/components/Calculator";
 
 const Subtract = () => {
-  return <div>Sub</div>;
+   const props = {
+    // 3
+    calculateFromUserInput: subtractRationalsFromUserInput,
+    title: "Subtract",
+    subtitle:
+      "Fill in the numerator and denominator of two rational numbers (fractions) below, then click the Calculate button to see the result.",
+    sign: "-",
+   }
+	return <Calculator {...props} />;
 };
 
 export default Subtract;
